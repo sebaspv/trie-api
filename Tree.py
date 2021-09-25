39e7e9516593124f4a42e19a77ab8d82d3f27f6b
Sebastian Ponce <poncegramming@gmail.com>
2021-09-25 00:44:27 -0500

    first commit

---
+++ Tree.py
@@ -0,0 +1,72 @@
+class Node:
+    '''
+    Trie node class.
+    Receives a string and yields its available next characters.
+    '''
+    def __init__(self):
+        self.words = {}
+        self.end = False
+    def search_words(self, prefix):
+        if self.end:
+            yield prefix # yield words with available prefix
+        # get autocompletion from root
+        for curr_char, child in self.words.items():
+            yield from child.search_words(prefix + curr_char)
+    
+class Trie:
+    '''
+    Trie search tree class.
+    All of the characters are composed of the Node() class.
+    '''
+    def __init__(self):
+        self.root = Node() # initialize tree from empty node
+
+    def add_word(self, word):
+        """Insert word to Trie"""
+        current = self.root
+        for char in word:
+            node = current.words.get(char)
+            if not node:
+                node = Node()
+                current.words[char] = node # add character to word set
+            current = node
+        current.end = True # end node when there are no more characters
+
+    def remove_word(self, word):
+        """Remove given word from Trie"""
+        current = self.root
+        if self.search_word(word):
+            for char in word:
+                current = current.words[char] # remove from root if word exists
+            current.end = False
+
+    def search_word(self, word): # from https://stackoverflow.com/questions/46038694/implementing-a-trie-to-support-autocomplete-in-python
+        """Search if word is in Trie"""
+        current = self.root
+        for char in word:
+            node = current.words.get(char) # get char from word searched
+            if not node:
+                return False # return false if word not in trie
+            current = node
+        return current.end # if word isn't finished, continue
+            
+    def prefix_word(self, prefix):
+        """Create word suggestions stored in Trie from a given prefix"""
+        curr = self.root
+        for char in prefix:
+            curr = curr.words.get(char) # search for character in trie
+            if curr is None:
+                return # return empty if no words have prefix
+        yield from curr.search_words(prefix) # yield words with prefix
+
+    def list_words(self, prefix):
+        """List stored words in Trie"""
+        for word in list(self.prefix_word(prefix)):
+            print(word)
+
+trie = Trie()
+trie.add_word("sebastian")
+trie.add_word("sebastiano")
+trie.add_word("sebas")
+trie.remove_word("sebastian")
+trie.list_words("s")
